f4d2c76d644ff9f8db3fd6909942faad25320b50
gribbans <ribbansg@yahoo.com>
2024-06-01 07:29:25 +0100

    COmmit all - big changes

---
+++ cyber-punk-futuretec-inspo-wall/javascript-ss-cy.js
@@ -0,0 +1,6 @@
+for (let i = 0; i <= 30; i++) {
+    const div = document.createElement('div');
+    div.style.backgroundImage = `url(/images-ss-cy/${i}.jpg)`;
+    document.body.appendChild(div);
+  }
+
